8158745df4b7e3b3d5f8413746bf6086319bf65c
uka-enkbold <uuganchimeg1104@gmail.com>
2022-08-16 21:11:50 -0500

    new commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -30,7 +30,7 @@ export class AppComponent {
 				 }
 
 				 // make a copy of the sourceList to be shown to the user
-				 this.displayList = this.sourceList.slice(0);
+				 this.displayList = this.sourceList.slice(Satellite);
 	  
 			}.bind(this));
 		}.bind(this));
